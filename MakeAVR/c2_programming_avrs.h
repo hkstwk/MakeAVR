0dc5a4c9d6b37961679451a1acd0989fba01db28
harm <harm@mbp-van-harm.dynamic.ziggo.nl>
2017-12-12 21:48:24 +0100

    Added blinkingLed anmimation in Chapter 2 files.

---
+++ MakeAVR/c2_programming_avrs.h
@@ -0,0 +1,15 @@
+/*
+ * c2_programming_avrs.h
+ *
+ *  Created on: 12 dec. 2017
+ *      Author: harm
+ */
+
+#ifndef C2_PROGRAMMING_AVRS_H_
+#define C2_PROGRAMMING_AVRS_H_
+
+void blinkingLed();
+void CountBinary();
+
+
+#endif /* C2_PROGRAMMING_AVRS_H_ */
